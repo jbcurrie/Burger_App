a82a7bc94e05436e228c78d95af2afc557773080
jbcurrie <biobacker30@gmail.com>
2017-09-06 10:57:24 -0400

    jaws db config

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  console.log(process.env.JAWSDB_URL);
+  console.log(`jaws connection: ${process.env.JAWSDB_URL}`);
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
     connection = mysql.createConnection({
